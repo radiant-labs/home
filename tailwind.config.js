8ef3584e9b85c5ed96989142c0565e4cadad685f
Nikhil Gupta <reach.guptanikhil@gmail.com>
2023-11-13 11:29:29 +0530

    Fixes alignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,9 @@ module.exports = {
           800: '#285E61',
           900: '#234E52',
         },
+        orange: {
+          100: '#DA552F',
+        }
       },
       boxShadow: {
         xs: '0 0 0 1px rgba(0, 0, 0, 0.16)',
